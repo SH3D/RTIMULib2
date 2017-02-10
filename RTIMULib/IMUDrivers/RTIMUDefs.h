09ff258d496b96e517b60ab2252bf79b6b5071df
Scott Penrose <scottp@dd.com.au>
2017-02-10 22:25:12 +0000

    Experimental change for 9255

--- RTIMULib/IMUDrivers/RTIMUDefs.h
+++ RTIMULib/IMUDrivers/RTIMUDefs.h
@@ -150,7 +150,7 @@
 
 #define MPU9250_ADDRESS0            0x68
 #define MPU9250_ADDRESS1            0x69
-#define MPU9250_ID                  0x71
+#define MPU9250_ID                  0x73		// XXX Scott Hack for 9255 MPU
 
 #define AK8963_ADDRESS              0x0c
 
